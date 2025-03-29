ce5cda07b0ded5023a5270c752fd9244fed30cb0
mistertayodimon <dimondimonych1@outlook.com>
2025-03-29 10:34:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="cartel",  
-    version="1.0.01",
+    version="1.1-a1",
     description="Как ему не страшно говорить про кортель",
     long_description=long_description,
     long_description_content_type="text/markdown",
