167320297629cf1996a42615f77be776ee471435
mistertayodimon <dimondimonych1@outlook.com>
2025-03-30 15:14:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,30 @@
+import os
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
-    long_description = f.read()
 
 setup(
-    name="cartel",  
-    version="1.1-a1",
-    description="Как ему не страшно говорить про кортель",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="mistertayodimon",  # Замените на своё имя
-    author_email="dimondimonych1@outlook.com",  # Замените на свой email
+    name='cartel',
+    version='1.1a2',
+    description='Как ему не страшно говорить про кортель. Инструкция: https://github.com/mistertay0dimon/cartel',
+    author='mistertayodimon',
+    author_email='dimondimonych1@outlook.com',
+    url='https://github.com/mistertay0dimon/cartel',
+    license='MIT',
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    install_requires=[
+        'colorama',
     ],
-    python_requires=">=3.6",  
-    # url="https://github.com/mistertay0dimon/cartel"
+    classifiers=[
+        'Development Status :: 3 - Alpha',  # Или другой статус разработки
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.6',
+    
 )
